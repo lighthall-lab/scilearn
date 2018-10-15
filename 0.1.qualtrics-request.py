f894f98175a961c4839a9438dfe61884f7ab4354
ianad <ian.dalton@ucf.edu>
2018-10-15 10:49:33 -0400

    added survey-list functionality; 0.1.qualtrics-request-list.txt contains survey IDs

--- 0.1.qualtrics-request.py
+++ 0.1.qualtrics-request.py
@@ -3,25 +3,40 @@
 
 # In[1]:
 
+from os.path import join
+from requests import request
+from time import sleep
+import zipfile, io, os, pandas as pd
 
-import os,io,requests,json,zipfile
+
+# In[2]:
 
 
-# In[9]:
+debug = False
 
 
-# Setting user & survey parameters
-apiToken = open(os.path.join('u:/','keys','qualtrics-api-token.txt'),'r').read()
-surveyIdList = ["SV_bsl9vF012E6Thbv","SV_bBNxUVhoGFcyKot","SV_2gAX1ty1UjOeKBT","SV_d4o0ZLBDB8GxpwV"]
+# # Setting user & survey parameters
+# Assumes user has api token generated and stored in text file at 'U:\keys\qualtrics-api-token.txt'
+# 
+# List as many or as few qualtrics survey IDs as needed in surveyIdList
+# 
+# Currently requires re-executing the script for each survey, as the zipfile extract fails out once per survey. I do not understand the zipfile functionality enough to troubleshoot at the moment.
+
+# In[3]:
+
+
+apiToken = open(join('u:\\','keys','qualtrics-api-token.txt'),'r').read()
+surveyIdList = list(filter(None,open('0.1.qualtrics-request-list.txt').read().split('\n')))
 fileFormat = "csv"
 dataCenter = 'ca1'
 output_dir = os.path.join('..','sourcedata','qualtrics')
 
 
-# In[10]:
+# # Setting static parameters
+
+# In[4]:
 
 
-# Setting static parameters
 requestCheckProgress = 0
 progressStatus = "in progress"
 baseUrl = "https://{0}.qualtrics.com/API/v3/responseexports/".format(dataCenter)
@@ -31,30 +46,87 @@ headers = {
     }
 
 
-# In[17]:
+# # Executing Request
+# If executing in Jupyter, this is the cell that will throw an error for each survey.
+# 
+# Simply re-execute until it stops throwing errors...
+
+# In[5]:
 
 
+i=1
 for surveyId in surveyIdList:
+    print('survey {} of {}'.format(i,len(surveyIdList)));i+=1
+    print('surveyId',surveyId)
+    
     # Step 1: Creating Data Export
     downloadRequestUrl = baseUrl
     downloadRequestPayload = '{"format":"' + fileFormat + '","surveyId":"' + surveyId + '"}'
-    downloadRequestResponse = requests.request("POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers)
+    downloadRequestResponse = request("POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers)
     progressId = downloadRequestResponse.json()["result"]["id"]
-    print(downloadRequestResponse.text)
+    
+    if debug == True:
+        print('downloadRequestUrl',downloadRequestUrl)
+        print('downloadRequestPayload',downloadRequestPayload)
+        print('downloadRequestResponse',downloadRequestResponse)
+        print('progressId',progressId)
     
     # Step 2: Checking on Data Export Progress and waiting until export is ready
-    while requestCheckProgress < 100 and progressStatus is not "complete":
+    while requestCheckProgress < 99 and progressStatus is not "complete":
         requestCheckUrl = baseUrl + progressId
-        requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
+        requestCheckResponse = request("GET", requestCheckUrl, headers=headers)
         requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
         print("Download is " + str(requestCheckProgress) + "% complete")
+    print("Download is " + str(requestCheckProgress) + "% complete")
     
+    if debug == True:
+        print('requestCheckUrl',requestCheckUrl)
+        print('requestCheckResponse',requestCheckResponse)
+        print('requestCheckProgress',requestCheckProgress)
+        
     # Step 3: Downloading file
     requestDownloadUrl = baseUrl + progressId + '/file'
-    requestDownload = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
+    sleep(2)
+    requestDownload = request("GET", requestDownloadUrl, headers=headers, stream=True)
+        
+    if debug == True:
+        print('requestDownloadUrl',requestDownloadUrl)
+        print('requestDownload',requestDownload)
     
     # Step 4: Unzipping the file
     zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall(output_dir)
-    print(surveyId,'done!')
-print('All done!')
+
+
+# # Read outputs, clean and re-write
+
+# In[6]:
+
+
+columns_to_drop=[
+    'StartDate','EndDate','Status','IPAddress','Finished',
+    'RecipientLastName','RecipientFirstName','RecipientEmail',
+    'LocationLatitude','LocationLongitude','LocationAccuracy',
+    'ResponseID','ResponseSet','ExternalDataReference',
+    'Score-weightedAvg','Score-weightedStdDev'
+]
+
+
+# In[7]:
+
+
+for f in os.listdir(output_dir):
+    fpath = os.path.join(output_dir,f)
+    if os.path.isfile(fpath):
+        print('cleaning {}'.format(fpath))
+        df = pd.read_csv(fpath)
+        df = df[df['Finished'] == '1']
+        df = df[[
+            c for c in df.columns
+            if not c.startswith('DO-')
+            and not c.startswith('RO-')
+            and not c in columns_to_drop
+        ]]
+        df.to_csv(fpath,index=False)
+        print('done')
+input('all done!')
 
