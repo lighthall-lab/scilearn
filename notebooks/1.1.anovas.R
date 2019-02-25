ef510da02c8982316d2e04e5f9037f86f4d3e536
ianad <ian.dalton@ucf.edu>
2019-02-25 11:16:33 -0500

    add post-hoc tests, reorganize

--- notebooks/1.1.anovas.R
+++ notebooks/1.1.anovas.R
@@ -1,4 +1,5 @@
-#install.packages("car")
+install.packages("car")
+setwd('\\\\net1110.net.ucf.edu/research2/lighthall_lab/experiments/scilearn/code')
 
 # Using `car` library for Anova() which allows Type III SumSq tests
 library(car)
@@ -16,29 +17,75 @@ levels(data$Condition) <- list("Annotated"=1,"Video"=2,"Original"=3)
 levels(data$AgeGroup) <- list("YA"=1,"OA"=2)
 levels(data$Gender) <- list("Male"=1,"Female"=2)
 
+# Subset data for Post-hoc One-way Condition ANOVAs within-AgeGroup
+oa_data = subset(data, AgeGroup == 'OA')
+ya_data = subset(data, AgeGroup == 'YA')
+
 # Set the contrasts for type III sum of squares
 options(contrasts = c("contr.sum", "contr.poly"))
 options("contrasts")
 
-# Two-way AgeGroup x Condition ANOVA on comprehension delta
-fit <- lm(comp_change_bound ~ AgeGroup * Condition, data)
-Anova(fit, type=3)
-#eta_sq(fit)
+
+
+
 
 # Two-way AgeGroup x Condition ANOVA on comprehension at T1
 fit <- lm(comp_t1_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
 
+# Comprehension T1 Post-hocs
+fit <- lm(comp_t1_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(comp_t1_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
+
 # Two-way AgeGroup x Condition ANOVA on comprehension at T2
 fit <- lm(comp_t2_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
 
+# Comprehension T2 post-hocs
+fit <- lm(comp_t2_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(comp_t2_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
+
+
+# Two-way AgeGroup x Condition ANOVA on comprehension delta
+fit <- lm(comp_change_bound ~ AgeGroup * Condition, data)
+Anova(fit, type=3)
+#eta_sq(fit)
+
+# Comprehension delta post-hocs
+fit <- lm(comp_change_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(comp_change_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
+
+
 # Two-way AgeGroup x Condition ANOVA on n-back reaction time
 fit <- lm(nb_RT_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
+
+# N-back RT post-hocs
+fit <- lm(nb_RT_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(nb_RT_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
 
 # Two-way AgeGroup x Condition ANOVA on n-back corrected recognition
 fit <- lm(nb_CoR_bound ~ AgeGroup * Condition, data)
@@ -52,10 +99,13 @@ fit <- lm(procspd_RT_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
 
-# Sampling Error Check: Processing Speed
-oa_data <- subset(data, AgeGroup == 'OA')
-fit <- lm(procspd_RT_bound ~ Condition, data = oa_data)
+# Proessing speed post-hocs
+fit <- lm(procspd_RT_bound ~ Condition, data=oa_data)
 Anova(fit, type=3)
+fit <- lm(procspd_RT_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
 
 
 
@@ -64,15 +114,59 @@ fit <- lm(vocab_sum_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
 
+# Vocab post-hocs
+fit <- lm(vocab_sum_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(vocab_sum_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
+
+
+
 # Two-way AgeGroup x Condition ANOVA on Need for Cognition score
 fit <- lm(NFCS_sum_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
 
+# NFCS Post-hocs
+fit <- lm(NFCS_sum_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(NFCS_sum_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
+
+
+
 # Two-way AgeGroup x Condition ANOVA on Trust in Science/Scientists score
 fit <- lm(TSSI_sum_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
+
+fit <- lm(TSSI_sum_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(TSSI_sum_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
+
+# Two-way AgeGroup x Condition ANOVA on Science Literacy
+fit <- lm(SciLit_sum_bound ~ AgeGroup * Condition, data)
+Anova(fit, type=3)
+#eta_sq(fit)
+
+# Science Literacy Post-hocs
+fit <- lm(SciLit_sum_bound ~ Condition, data=oa_data)
+Anova(fit, type=3)
+fit <- lm(SciLit_sum_bound ~ Condition, data=ya_data)
+Anova(fit, type=3)
+
+
+
 
 # Two-way AgeGroup x Condition ANOVA on Science Attitudes subscale-A
 fit <- lm(SciTudeA_sum_bound ~ AgeGroup * Condition, data)
@@ -89,7 +183,6 @@ fit <- lm(SciTudeS_sum_bound ~ AgeGroup * Condition, data)
 Anova(fit, type=3)
 #eta_sq(fit)
 
-# Two-way AgeGroup x Condition ANOVA on Science Literacy
-fit <- lm(SciLit_sum_bound ~ AgeGroup * Condition, data)
-Anova(fit, type=3)
-#eta_sq(fit)
+
+
+
